fb39a7c008e3a1466417433bb34a99566cd426e9
Anders Pitman <tapitman11@gmail.com>
2019-11-24 09:54:53 -0700

    Multiple workers for all served files

--- host.sh
+++ host.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+#set -x
 
 rootChannel=$1
 token="dummytoken"
@@ -15,12 +15,24 @@ trap 'kill $(jobs -p)' EXIT
 function hostFile {
         local channel=$1
         local path=$2
+
+        for workerId in {1..4}
+        do
+                echo "Starting worker $workerId for $path"
+                hostFileWorker $channel $path $workerId &
+        done
+
+        wait
+}
+
+function hostFileWorker {
+        local channel=$1
+        local path=$2
         local workerId=$3
 
         while true
         do
-                curl -X POST -H "Authorization: Bearer $token" $rootChannel$channel --data-binary @$path
-                echo $workerId
+                curl -s -X POST -H "Authorization: Bearer $token" $rootChannel$channel --data-binary @$path
 
                 if [ "$?" -ne "0" ]; then
                         sleep 1
@@ -32,14 +44,17 @@ function receiveCurrentPage {
 
         while true
         do
-                val=$(curl $rootChannel/set-current-page)
+                val=$(curl -s $rootChannel/set-current-page)
                 printf $val > ./current_page.txt
 
                 # consume the stale current-page value
-                curl $rootChannel/current-page &
+                for workerId in {1..4}
+                do
+                        curl -s $rootChannel/current-page > /dev/null &
+                done
 
                 # broadcast new value
-                printf "data: $(<./current_page.txt)\n\n" | curl $rootChannel/current-page?pubsub=true --data-binary @- &
+                printf "data: $(<./current_page.txt)\n\n" | curl -s $rootChannel/current-page?pubsub=true --data-binary @- &
 
                 if [ "$?" -ne "0" ]; then
                         sleep 1
@@ -52,20 +67,10 @@ hostFile /slide_deck.js ./slide_deck.js &
 hostFile /slide_deck.css ./slide_deck.css &
 hostFile /current-page ./current_page.txt &
 
-hostFile /meme.jpg ./meme.jpg 1 &
-hostFile /meme.jpg ./meme.jpg 2 &
-hostFile /meme.jpg ./meme.jpg 3 &
-hostFile /meme.jpg ./meme.jpg 4 &
-
-hostFile /elder_uchtdorf.jpg ./elder_uchtdorf.jpg 1 &
-hostFile /elder_uchtdorf.jpg ./elder_uchtdorf.jpg 2 &
-hostFile /elder_uchtdorf.jpg ./elder_uchtdorf.jpg 3 &
-hostFile /elder_uchtdorf.jpg ./elder_uchtdorf.jpg 4 &
-
-hostFile /qr.svg ./qr.svg 1 &
-hostFile /qr.svg ./qr.svg 2 &
-hostFile /qr.svg ./qr.svg 3 &
-hostFile /qr.svg ./qr.svg 4 &
+# images
+hostFile /meme.jpg ./meme.jpg &
+hostFile /elder_uchtdorf.jpg ./elder_uchtdorf.jpg &
+hostFile /qr.svg ./qr.svg &
 
 receiveCurrentPage &
 
