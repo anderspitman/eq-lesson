51b0a44d828a9f6c13e2bcf41ae0c7ba7d0f5226
Anders Pitman <tapitman11@gmail.com>
2019-11-23 19:25:17 -0700

    Add a few more slides

--- index.js
+++ index.js
@@ -41,11 +41,174 @@ And yet, the call to adventure has reached deep into his heart. And so, this unr
   return dom;
 };
 
+const TerrifiedSlide = () => {
+  const dom = document.createElement('div');
+  dom.innerHTML = `
+    <p>
+      There must have been parts of the mortal adventure that worried and even
+      terrified God’s children, since a large number of our spiritual brothers
+      and sisters decided against it.
+    </p>
+  `;
+  return dom;
+};
 
+const DistractionsSlide = () => {
+  const dom = document.createElement('div');
+  dom.innerHTML = `
+  <p>
+And yet, mortal life has a way of distracting us, doesn’t it? We tend to lose sight of our great quest, preferring comfort and ease over growth and progress.
+
+</p>
+<p>
+Still, there remains something undeniable, deep within our hearts, that hungers for a higher and nobler purpose. This hunger is one reason why people are drawn to the gospel and Church of Jesus Christ. The restored gospel is, in a sense, a renewal of the call to adventure we accepted so long ago. The Savior invites us, each day, to set aside our comforts and securities and join Him on the journey of discipleship.
+
+</p>
+<p>
+There are many bends in this road. There are hills, valleys, and detours. There may even be metaphorical spiders, trolls, and even a dragon or two. But if you stay on the path and trust in God, you will eventually find the way to your glorious destiny and back to your heavenly home.
+</p>
+  `;
+  return dom;
+};
+
+const DiscipleshipSlide = () => {
+  const dom = document.createElement('div');
+  dom.innerHTML = `
+    <p>
+    If you hesitate in this adventure because you doubt your ability, remember that discipleship is not about doing things perfectly; it’s about doing things intentionally. It is your choices that show what you truly are, far more than your abilities.
+    </p>
+  `;
+  return dom;
+};
+
+const ReachOutSlide = () => {
+  const dom = document.createElement('div');
+  dom.innerHTML = `
+  <p>
+There is something interesting, almost paradoxical, about this path you’ve chosen: the only way for you to progress in your gospel adventure is to help others progress as well.
+  </p>
+  <p>
+To help others is the path of discipleship. Faith, hope, love, compassion, and service refine us as disciples.
+  </p>
+  <p>
+Through your efforts to help the poor and the needy, to reach out to those in distress, your own character is purified and forged, your spirit is enlarged, and you walk a little taller.
+  </p>
+  <p>
+But this love cannot come with expectations of repayment. It cannot be the kind of service that expects recognition, adulation, or favor.
+  </p>
+  <p>
+True disciples of Jesus Christ love God and His children without expectation of something in return. We love those who disappoint us, who don’t like us. Even those who ridicule, abuse, and seek to hurt us.
+  </p>
+  <p>
+When you fill your hearts with the pure love of Christ, you leave no room for rancor, judgment, and shaming. You keep God’s commandments because you love Him. In the process, you slowly become more Christlike in your thoughts and deeds. And what adventure could be greater than this?
+  </p>
+  `;
+  return dom;
+};
+
+const NoSellSlide = () => {
+  const dom = document.createElement('div');
+  dom.innerHTML = `
+  <p>
+May I remind you that God does not need you to “sell” the restored gospel or the Church of Jesus Christ.
+</p>
+
+<p>
+He simply expects you not to hide it under a bushel.
+</p>
+  `;
+  return dom;
+};
+
+const TreeOfLifeSlide = () => {
+  const dom = document.createElement('div');
+  dom.innerHTML = `
+    <h1>1 Nephi 8:12</h1>
+
+    <p>
+      And as I partook of the fruit thereof it filled my soul with exceedingly great joy; wherefore, I began to be desirous that my family should partake of it also; for I knew that it was desirable above all other fruit.
+    </p>
+  `;
+  return dom;
+};
+
+const ItWorksExcerptSlide = () => {
+  const dom = document.createElement('div');
+  dom.innerHTML = `
+  <h1>Excerpt from "It Works Wonderfully!" talk</h1>
+  <p>
+When it comes to spiritual truth, how can we know that we are on the right path?
+</p>
+<p>
+
+One way is by asking the right questions﻿—the kind that help us ponder our progress and evaluate how things are working for us. Questions like:
+
+</p>
+<p>
+“Does my life have meaning?”
+
+</p>
+<p>
+“Do I believe in God?”
+
+</p>
+<p>
+“Do I believe that God knows and loves me?”
+
+</p>
+<p>
+“Do I believe that God hears and answers my prayers?”
+
+</p>
+<p>
+“Am I truly happy?”
+
+</p>
+<p>
+“Are my efforts leading me to the highest spiritual goals and values in life?”
+
+</p>
+<p>
+Profound questions regarding the purpose of life have led many individuals and families throughout the world to search for truth. Often that search has led them to The Church of Jesus Christ of Latter-day Saints and to the restored gospel.
+
+</p>
+<p>
+I wonder if we as Church members might also benefit from asking ourselves from time to time: “Is my experience in the Church working for me? Is it bringing me closer to Christ? Is it blessing me and my family with peace and joy as promised in the gospel?”
+
+</p>
+<p>
+Alma posed similar questions to Church members in Zarahemla when he asked: “Have ye experienced this mighty change in your hearts? … [And] can [you] feel [it] now?” Such contemplation may help us to refocus or realign our daily efforts with the divine plan of salvation.
+
+</p>
+<p>
+Many members will answer with great warmth that their experience as a member of the Church is working exceptionally well for them. They will testify that whether during times of poverty or prosperity, whether things are pleasant or painful, they find great meaning, peace, and joy because of their commitment to the Lord and their dedicated service in the Church. Every day I meet Church members who are filled with a radiant joy and who demonstrate in word and deed that their lives are immeasurably enriched by the restored gospel of Jesus Christ.
+
+</p>
+<p>
+But I also recognize that there are some who have a less-than-fulfilling experience﻿—who feel that their membership in the Church sometimes isn’t quite what they had hoped for.
+
+</p>
+<p>
+This saddens me because I know firsthand how the gospel can invigorate and renew one’s spirit﻿—how it can fill our hearts with hope and our minds with light. I know for myself how the fruits of the gospel of Jesus Christ can transform lives from the ordinary and dreary to the extraordinary and sublime.
+
+</p>
+<p>
+But why does it seem to work better for some than for others? What is the difference between those whose experience in the Church fills their souls with songs of redeeming love and those who feel that something is lacking?
+</p>
+  `;
+  return dom;
+};
 const slides = [
   { id: 'qr', component: QrSlide },
   { id: 'meme', component: MemeSlide },
   { id: 'bilbo', component: BilboSlide },
+  { id: 'terrified', component: TerrifiedSlide },
+  { id: 'distractions', component: DistractionsSlide },
+  { id: 'discipleship', component: DiscipleshipSlide },
+  { id: 'reach-out', component: ReachOutSlide },
+  { id: 'no-sell', component: NoSellSlide },
+  { id: 'tree-of-life', component: TreeOfLifeSlide },
+  { id: 'it-works-excerpt', component: ItWorksExcerptSlide },
 ];
 
 let curSlideIndex = 0;
